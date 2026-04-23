271b839aa7f34ed182c89e4e1ec3a6913a636a98
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-04-23 04:14:55 +0300

    Edit tests for Home component

--- src/components/home/tests/Home.test.jsx
+++ src/components/home/tests/Home.test.jsx
@@ -2,12 +2,15 @@ import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { MemoryRouter } from "react-router";
 import Home from "../Home";
+import AuthProvider from "../../../providers/AuthProvider";
 
 describe("Home component snapshot", () => {
   it("Renders correctly", async () => {
     const { container } = render(
       <MemoryRouter>
-        <Home />
+        <AuthProvider>
+          <Home />
+        </AuthProvider>
       </MemoryRouter>,
     );
 
