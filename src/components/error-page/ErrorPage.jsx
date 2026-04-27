6cc8d8fc4ff0a11b7918cf21ac5276866c93d411
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-04-27 05:46:11 +0300

    Add ErrorPage component

---
+++ src/components/error-page/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import styles from "./ErrorPage.module.css";
+import alertIconUrl from "../../assets/icons/alert-rhombus.svg";
+import { Link } from "react-router";
+
+export default function ErrorPage({ errorTitle, errorMessage }) {
+  return (
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <img src={alertIconUrl} alt="Erorr alert!" />
+        <h1>{errorTitle}</h1>
+        <p>{errorMessage}</p>
+        <Link to="/" className={styles.link}>
+          Back to Homepage
+        </Link>
+      </section>
+    </div>
+  );
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 import App from "./components/app/App";
+import ErrorPage from "./components/error-page/ErrorPage";
 import Home from "./components/home/Home";
 import Posts from "./components/posts/Posts";
 
@@ -6,6 +7,12 @@ const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: (
+      <ErrorPage
+        errorTitle="Page Not Found"
+        errorMessage="The page you're looking for doesn't exist or has been moved."
+      />
+    ),
     children: [
       { index: true, element: <Home /> },
       { path: "posts", element: <Posts /> },
